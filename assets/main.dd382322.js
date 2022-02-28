e9af94bba6fbccf5687dfaf5c3cfc5831f9d7e7f
JamesODonoghue <JamesODonoghue@users.noreply.github.com>
2022-02-28 05:30:27 +0000

    deploy: 2e8df51af7d403b4346a712940bbd8e4f3a31070

--- assets/main.dd382322.js
+++ assets/main.dd382322.js
@@ -1,11 +1,11 @@
-import{c as a,L as c,a as d,h as f}from"./vendor.16442617.js";const u=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}};u();var p="/vr-web-app/assets/rocket.86cef859.glb",m=Object.defineProperty,v=Object.getOwnPropertyDescriptor,h=(n,t,i,s)=>{for(var e=s>1?void 0:s?v(t,i):t,r=n.length-1,o;r>=0;r--)(o=n[r])&&(e=(s?o(t,i,e):o(e))||e);return s&&e&&m(t,i,e),e};let l=class extends c{static get styles(){return d`
+import{c as a,L as c,a as d,h as f}from"./vendor.16442617.js";const u=function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}};u();var p="/vr-web-app/assets/rocket.86cef859.glb",m=Object.defineProperty,v=Object.getOwnPropertyDescriptor,h=(n,t,o,s)=>{for(var e=s>1?void 0:s?v(t,o):t,r=n.length-1,i;r>=0;r--)(i=n[r])&&(e=(s?i(t,o,e):i(e))||e);return s&&e&&m(t,o,e),e};let l=class extends c{static get styles(){return d`
             .main {
                 display: flex;
             }
 
             .main__left {
                 width: 50%;
-                text-align: center;
+                text-align: right;
             }
 
             h1 {
