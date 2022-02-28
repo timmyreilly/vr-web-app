162d60efcbd7aa00011f6cc9042fa978a5363287
James O'Donoghue <jameso@Astraits-MacBook-Pro-5.local>
2022-02-28 10:26:34 -0800

    feat: support ar modes

--- src/app.ts
+++ src/app.ts
@@ -38,18 +38,23 @@ export class VrApp extends LitElement {
                         style="height: 100%; width: 100%; position:absolute"
                         bounds="tight"
                         src=${rocketUrl}
+                        ar
                         ar-modes="webxr scene-viewer quick-look"
                         camera-controls
+                        environment-image="neutral"
                         shadow-intensity="1"
-                        autorotate
                     >
                         <div class="progress-bar hide" slot="progress-bar">
                             <div class="update-bar"></div>
                         </div>
-
                         <button slot="ar-button" id="ar-button">
                             View in your space
                         </button>
+                        <div id="ar-prompt">
+                            <img
+                                src="https://modelviewer.dev/shared-assets/icons/hand.png"
+                            />
+                        </div>
                     </model-viewer>
                 </div>
             </div>
